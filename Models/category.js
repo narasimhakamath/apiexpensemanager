b5955c5b370749da6151ccb798598b1925879de5
Narasimha Kamath <narasimhakamath@outlook.com>
2020-04-17 02:56:13 +0530

    Created Category model.

---
+++ Models/category.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	name: {type: String, required: true, unique: true}
+});
+
+
+
+
+
+module.exports = mongoose.model("Category", categorySchema);

bfbad112da394a8bb2fabcdec2463723b6efa5cb
Narasimha Kamath <narasimhakamath@outlook.com>
2020-04-17 19:48:43 +0530

    Created a middleware to check if the user is logged in to perform any actions.

---
+++ Middlewares/checkAuthentication.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (request, response, next) => {
+	let responseData = {statusCode: 401, success: "", message: "Authentication failed. You do not have permissions to perform this action."};
+
+	if(request['headers']['authorization']) {
+		const token = request['headers']['authorization'];
+		try {
+			const decodedData = jwt.verify(token, "secret", null);
+			console.log(decodedData);
+			request['userData'] = decodedData;
+			next();
+		} catch(error) {
+			return response.status(responseData.statusCode).json(responseData);
+		}
+	} else {
+		return response.status(responseData.statusCode).json(responseData);
+	}
+}
--- Routes/categories.js
+++ Routes/categories.js
@@ -3,15 +3,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+const checkAuthentication = require("../Middlewares/checkAuthentication");
+
 const router = express.Router();
 
 const CategoriesController = require("./../Controllers/categories");
 
 // GET API to get all the categories.
-router.get("/getAllCategories", CategoriesController.getAllCategories);
+router.get("/getAllCategories", checkAuthentication, CategoriesController.getAllCategories);
 
 // POST API to create a new category.
-router.post("/createCategory", CategoriesController.createCategory);
+router.post("/createCategory", checkAuthentication, CategoriesController.createCategory);
 
 
 module.exports = router;
